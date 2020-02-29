5f24145f1cfec756923ae70b6ca3ad8b512e566d
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 17:26:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["jessica", "jennifer"]
+  array = ["wow", "I", "am", "really", "learning"]
    element = "jamie"
   array << element 
   p array 
