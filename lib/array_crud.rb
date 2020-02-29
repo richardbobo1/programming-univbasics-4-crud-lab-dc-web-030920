4d23ca23e52d70402398da2c3f682658e00474ca
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 17:30:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,10 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+
+  array = ["wow", "I", "am", "really", "learning"]
+  array << element 
+
 end
 
 def remove_element_from_start_of_array(array)
