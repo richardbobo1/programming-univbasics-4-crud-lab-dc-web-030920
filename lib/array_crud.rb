1fdbfcad2dc9bd9aa8c3685cae9e42be067f731e
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 17:35:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,8 +34,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-   index_number = -1
+   array = ["I", "am", "really", "learning", "arrays!"]
+   index_number = 2
    array[index_number]
   
 end
