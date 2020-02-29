9055225eef198a065bae60c4e9f8547fde6548c5
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 17:28:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
+  array = ["I", "am", "really", "learning"]
+   element = "arrays!"
+  array << element 
+
 end
 
 def remove_element_from_end_of_array(array)
